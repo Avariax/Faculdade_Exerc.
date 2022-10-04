3d489b17a20f8d5f439d00f92d82979f74525793
Pedro Henrique <104384284+Pedromermo@users.noreply.github.com>
2022-10-03 21:37:50 -0300

    Create Calculo_de_media.php

---
+++ Exercicios-PHP/Cálculo de média e situação de aluno/Calculo_de_media.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<h1 style="text-align:center;">Calculadora de Média</h1>
+
+<form action="Media.php" method="GET" style="text-align:center;">
+  <label for="fname">Nome:</label>
+    <input type="text" id="nome" name="nome"><br><br>
+  <label for="lname">Nota do 1 Bimestre:</label>
+    <input type="" id="nota1" name="nota1"><br><br>
+<label for="lname">Nota do 2 Bimestre:</label>
+    <input type="" id="nota2" name="nota2"><br><br> 
+  <input type="submit" value="Calcular">
+</form>
+
+</body>
+</html>
