6c20fa21d702fe62550c5b93b6f6f0ccca7bf562
Pedro Henrique <104384284+Pedromermo@users.noreply.github.com>
2022-10-03 21:43:33 -0300

    Create Cálculo do IMC.php

---
+++ Exercicios-PHP/Cálculo do IMC e Cálculo de média e situação de aluno/Cálculo do IMC/Cálculo do IMC.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IMC Cálculo</title>
+</head>
+<body>
+    <h1 style="text-align:center;">Calculadora de IMC</h1>
+
+<form action="IMC.php" method="GET" style="text-align:center;">
+  <label for="fname">Peso:</label>
+    <input type="number" id="peso" name="peso"><br><br>
+  <label for="lname">Altura:</label>
+    <input type="" id="altura" name="altura"><br><br>
+  <input type="submit" value="submit" name="Calcular">
+</form>
+    
+</body>
+</html>
